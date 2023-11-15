4a8c0ffe6c1b5617661fa5a0318fde1eb17eb8b3
justin <justin@jle.im>
2023-11-14 16:26:22 -0800

    nixconfig

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -96,6 +96,15 @@
               "args": ["--lsp"],
               "rootPatterns": ["*.cabal", "stack.yaml", "cabal.project", "package.yaml", "hie.yaml"],
               "filetypes": ["haskell", "lhaskell"]
+            },
+            "nix": {
+              "command": "nil",
+              "rootPatterns": ["flake.nix"],
+              "settings": {
+                "nil": {
+                  "formatting": { "command": ["nixpkgs-fmt"] }
+                }
+              }
             }
           }
         }
