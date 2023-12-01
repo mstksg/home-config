297be1cc73285ee45be90abcab3e1f439b5d641b
justin <justin@jle.im>
2023-12-01 12:19:07 -0800

    fix cocconifg

--- modules/editing.nix
+++ modules/editing.nix
@@ -253,14 +253,6 @@ in
                 }
               }
             }
-            "pandoc": {
-              "settings": {
-                "purescript": {
-                  "addSpagoSources": true,
-                  "addNpmPath": true, // Set to true if using a local purty install for formatting
-                  "formatter": "purty"
-                }
-              }
           }
         }
       '';
