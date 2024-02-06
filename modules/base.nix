038de557716870f37c379139e4f808fa4035a018
justin <justin@jle.im>
2024-02-05 20:29:12 -0800

    whats up

--- modules/base.nix
+++ modules/base.nix
@@ -28,7 +28,7 @@ in {
       default = false;
     };
     extraTmuxWindows = lib.mkOption {
-      type = lib.types.list;
+      type = lib.types.listOf lib.types.attrs;
       description = "Extra tmux windows to load on the default pane, in tmuxp format";
       default = [ ];
     };
