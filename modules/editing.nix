d0fbd13b4c5cd761636148e26a91545d2bc4739a
justin <justin@jle.im>
2024-02-12 20:27:14 -0800

    blippi

--- modules/editing.nix
+++ modules/editing.nix
@@ -486,6 +486,7 @@ in
           " nnoremap <CR> :nohlsearch<CR>
           nnoremap <c-p> :FZF<CR>
           cabbrev wq w
+          tnoremap <C-\> <C-\><C-n>:bp<CR>
 
           noremap <leader>tt :tn<CR>
           noremap <leader>tp :tN<CR>
