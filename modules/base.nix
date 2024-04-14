ab14b3a98158eae09ba7ed8ff36524c669011a71
justin <justin@jle.im>
2024-04-14 15:51:31 -0700

    we should always have octave

--- modules/base.nix
+++ modules/base.nix
@@ -61,25 +61,6 @@ in
       # The home.packages option allows you to install Nix packages into your
       # environment.
       home.packages = builtins.filter checkPlatform [
-        # # Adds the 'hello' command to your environment. It prints a friendly
-        # # "Hello, world!" when run.
-        # pkgs.hello
-
-        # # It is sometimes useful to fine-tune packages, for example, by applying
-        # # overrides. You can do that directly here, just don't forget the
-        # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-        # # fonts?
-        # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-        # # You can also create simple shell scripts directly inside your
-        # # configuration. For example, this adds a command 'my-hello' to your
-        # # environment:
-        # (pkgs.writeShellScriptBin "my-hello" ''
-        #   echo "Hello, ${config.home.username}!"
-        # '')
-
-        # TODO: some of these can be moved out of basic
-
         pkgs.binutils
         pkgs.cachix
         pkgs.cmatrix
@@ -97,6 +78,7 @@ in
         pkgs.nixfmt-classic
         pkgs.nixpkgs-fmt
         pkgs.nodejs
+        pkgs.octave
         pkgs.ripgrep
         pkgs.rogue
         pkgs.sl
